68c46a941f0809b1ba0c4a1c8aaeeeb55af275bd
JayXani <danuloxaraujo@gmail.com>
2023-10-14 13:22:26 -0300

    Created the repository of user.

--- todolist/src/main/java/br/com/apirest/todolist/users/UserController.java
+++ todolist/src/main/java/br/com/apirest/todolist/users/UserController.java
@@ -1,5 +1,8 @@
 package br.com.apirest.todolist.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user") //This is generally a route main.
 public class UserController {
+    @Autowired
+    private IUserRepository userRepository;
     @PostMapping("/")
-    public void create(@RequestBody UserModel user){
-
+    public ResponseEntity create(@RequestBody UserModel user){
+        var userExist = this.userRepository.findByFullName(user.getFullName());
+        if(userExist != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists in Data base");
+        }
+        var userCreated = this.userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body("User created !\n");
     }
 }
--- todolist/src/main/java/br/com/apirest/todolist/users/UserModel.java
+++ todolist/src/main/java/br/com/apirest/todolist/users/UserModel.java
@@ -1,5 +1,29 @@
 package br.com.apirest.todolist.users;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_users") // Declared UserModel who table in database
 public class UserModel {
-    public String name;
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
+    private String fullName;
+
+    private int age;
+    private String password;
+    @CreationTimestamp
+    private LocalDateTime created_at;
+    @CreationTimestamp
+    private LocalDateTime updated_at;
 }
